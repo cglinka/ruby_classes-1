a97d6e7fa82b0d8faa115cc28da2b73d206e36cc
Clare Glinka <glinka.cb@gmail.com>
2013-09-05 15:36:53 -0500

    Add car.rb, a method that creats a Car class, and methods for driving, fueling, and checking the car's status.

---
+++ car.rb
@@ -0,0 +1,33 @@
+class Car
+	def initialize
+		@fuel = 10
+		@distance = 0
+		puts "the initialize method is running automatically."	
+	end
+
+	def get_info
+		"I'm a car! I've driven #{@distance} miles and have #{@fuel} gallons of gas left."		
+	end
+
+	def drive(miles_driven)
+		testdistance = @distance + miles_driven
+		testfuel = @fuel - (miles_driven/20.0)
+		if testfuel > 0
+			@distance = @distance + miles_driven
+			@fuel = @fuel - (miles_driven/20.0)
+			puts "You drove #{miles_driven} and have #{@fuel} gallons left."
+		else
+			drivable_distance = @fuel * 20.0
+			@distance = @distance + drivable_distance
+			@fuel = @fuel - (drivable_distance/20)
+			puts "You drove #{drivable_distance} before you ran out of gas. You have #{@fuel} gallons."
+		end
+	end
+
+	def fuel_up
+		fuel_needed = 10 - @fuel
+		cost = fuel_needed * 3.5
+		puts "You need #{fuel_needed} gallson to fill up your tank. That'll be #{cost} dollars."
+		@fuel = @fuel + fuel_needed
+	end
+ end
