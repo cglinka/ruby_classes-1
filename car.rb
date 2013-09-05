d5164a273ebb1378f447703af259b8c956059098
Clare Glinka <glinka.cb@gmail.com>
2013-09-05 17:00:43 -0500

    Fix grammar, swith equation syntax.

--- car.rb
+++ car.rb
@@ -12,21 +12,21 @@ class Car
 	def drive(miles_driven)
 		testfuel = @fuel - (miles_driven/20.0)
 		if testfuel > 0
-			@distance = @distance + miles_driven
+			@distance += miles_driven
 			@fuel = testfuel
-			puts "You drove #{miles_driven} and have #{@fuel} gallons left."
+			puts "You drove #{miles_driven} miles and have #{@fuel} gallons of gas left."
 		else
 			drivable_distance = @fuel * 20.0
-			@distance = @distance + drivable_distance
-			@fuel = @fuel - (drivable_distance/20)
-			puts "You drove #{drivable_distance} before you ran out of gas. You have #{@fuel} gallons."
+			@distance += drivable_distance
+			@fuel -= (drivable_distance/20)
+			puts "You drove #{drivable_distance} miles before you ran out of gas. You have #{@fuel} gallons of gas left."
 		end
 	end
 
 	def fuel_up
 		fuel_needed = 10 - @fuel
 		cost = fuel_needed * 3.5
-		puts "You need #{fuel_needed} gallson to fill up your tank. That'll be #{cost} dollars."
-		@fuel = @fuel + fuel_needed
+		puts "You need #{fuel_needed} gallons to fill up your tank. That'll be #{cost} dollars."
+		@fuel += fuel_needed
 	end
  end
